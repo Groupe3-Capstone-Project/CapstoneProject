16b1758af46c3a48bcfcf03b5fc012348dee640e
GabeLawFR <gabriel.lawson@live.fr>
2023-09-19 20:06:04 -0700

    initial structure

---
+++ src/client/components/MainContainer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+
+
+
+export default function MainContainer() {
+    return (
+        <div>
+            <Routes>
+                <Route></Route>
+                <Route></Route>
+                <Route></Route>
+            </Routes>
+        </div>
+    )
+}
